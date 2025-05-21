ebcf9d7396243456cc36dd6b3af40411e3c49cd6
alimuzzaman-zamaddar <alimuzzamanzamaddar@gmail.com>
2025-05-21 10:16:13 +0600

    done filter sidebar

--- src/Pages/Products.tsx
+++ src/Pages/Products.tsx
@@ -1,10 +1,11 @@
 import { FaAngleDown } from "react-icons/fa";
 import Container from "../Components/Shared/Container";
 import { Link, useNavigate } from "react-router-dom";
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Range, getTrackBackground } from "react-range";
 import PageHeading from "@/Components/Reusable/PageHeading";
 import ReactPaginate from "react-paginate";
+import { IoFilter } from "react-icons/io5";
 
 const Products = () => {
   const navigate = useNavigate();
@@ -779,8 +780,58 @@ const Products = () => {
   const [selectedSubCategory, setSelectedSubCategory] = useState<string | null>(
     null
   );
+  const [showModal, setShowModal] = useState(false);
+  const [isExiting, setIsExiting] = useState(false);
+  const modalRef = useRef<HTMLDivElement | null>(null);
   const [sortOption, setSortOption] = useState<string | null>(null);
   const [isSortOpen, setIsSortOpen] = useState(false);
+  // Toggle scroll on body based on modal state
+  useEffect(() => {
+    if (showModal) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+
+    // Cleanup when component unmounts
+    return () => document.body.classList.remove("overflow-hidden");
+  }, [showModal]);
+
+  const handleCloseModal = () => {
+    setIsExiting(true);
+    setTimeout(() => {
+      setShowModal(false);
+      setIsExiting(false);
+    }, 400);
+  };
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (
+        modalRef.current &&
+        !modalRef.current.contains(event.target as Node)
+      ) {
+        handleCloseModal();
+      }
+    }
+
+    if (showModal) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [showModal]);
+
+  useEffect(() => {
+    if (showModal) {
+      document.body.classList.add("overflow-hidden");
+    } else {
+      document.body.classList.remove("overflow-hidden");
+    }
+    return () => document.body.classList.remove("overflow-hidden");
+  }, [showModal]);
 
   const [currentPage, setCurrentPage] = useState(1);
   const itemsPerPage = 6;
@@ -816,14 +867,31 @@ const Products = () => {
     <>
       <Container className=" pb-15 2xl:pb-25  mt-20">
         <div className="px-5 2xl:px-0">
-          <PageHeading
-            title="All Products"
-            subtitle="Everything You Need, All in One Place"
-          />
-          <main className="px-5">
+          <div className="flex justify-between items-baseline">
+            <div className="">
+              <PageHeading
+                title="All Products"
+                subtitle="Everything You Need, All in One Place"
+              />
+            </div>
+            <div className="lg:hidden block">
+              <button
+                onClick={() => setShowModal(true)}
+                className="flex gap-2 items-center cursor-pointer "
+              >
+                Filter{" "}
+                <span>
+                  {" "}
+                  <IoFilter />
+                </span>
+              </button>
+            </div>
+          </div>
+
+          <main className="">
             <div className="lg:flex gap-14">
               {/* Sidebar Filters */}
-              <div className="col-span-12 lg:col-span-3 space-y-6">
+              <div className="col-span-12 lg:col-span-3 space-y-6 hidden lg:block">
                 {/* Price Range */}
                 <div className="w-full mt-10">
                   <h3
@@ -1088,6 +1156,187 @@ const Products = () => {
             </div>
           </main>
         </div>
+
+        {showModal && (
+          <div className="fixed inset-0 z-50  bg-black/10 bg-opacity-50">
+            <div
+              ref={modalRef}
+              className={`bg-white p-6 max-w-[300px] w-full relative h-[100vh] overflow-y-auto scroll-smooth ${
+                isExiting ? "animate-slide-out-left" : "animate-slide-in-left"
+              }`}
+            >
+              <button
+                onClick={handleCloseModal}
+                className="absolute top-2 right-2 text-black hover:text-red-500 text-xl font-bold"
+              >
+                &times;
+              </button>
+              <div className="col-span-12 lg:col-span-3 space-y-6">
+                {/* Price Range */}
+                <div className="w-full mt-10">
+                  <h3 className=" not-italic text-black font-semibold leading-none text-lg sm:text-xl md:text-2xl lg:text-[28px] xl:text-[32px] mb-4">
+                    Price Range
+                  </h3>
+
+                  <Range
+                    values={values}
+                    step={5}
+                    min={MIN}
+                    max={MAX}
+                    onChange={(values) => setValues(values)}
+                    renderTrack={({ props, children }) => (
+                      <div
+                        {...props}
+                        style={{
+                          ...props.style,
+                          height: "6px",
+                          width: "100%",
+                          background: getTrackBackground({
+                            values,
+                            colors: ["#B2B2B2", "#000000", "#B2B2B2"],
+                            min: MIN,
+                            max: MAX,
+                          }),
+                          borderRadius: "8px",
+                          margin: "3.5rem 0",
+                        }}
+                      >
+                        {children}
+                      </div>
+                    )}
+                    renderThumb={({ props, index }) => (
+                      <div
+                        {...props}
+                        className="w-5 h-5 bg-black rounded-full cursor-pointer"
+                      >
+                        <div className="absolute top-[-30px] text-xs bg-white px-2 py-1 rounded shadow">
+                          ${values[index]}
+                        </div>
+                      </div>
+                    )}
+                  />
+
+                  {/* Price labels below */}
+                  <div className="flex justify-between gap-6 text-sm text-black font-semibold">
+                    <span className="flex items-center gap-2 px-6 py-1 lg:px-12 lg:py-3 rounded-[24px] bg-[#F5F5F5]">
+                      ${values[0]}
+                    </span>
+                    <span className="flex items-center gap-2 px-6 py-1 lg:px-12 lg:py-3 rounded-[24px] border border-black">
+                      ${values[1]}
+                    </span>
+                  </div>
+                </div>
+
+                {/* Sort By */}
+                <div className="w-full flex items-center justify-center relative">
+                  <button
+                    onClick={() => setIsSortOpen(!isSortOpen)}
+                    className="w-full flex justify-center items-center cursor-pointer px-2 py-2  2xl:px-3 2xl:py-[20px] rounded-[24px] bg-white text-black text-lg font-semibold border border-black shadow-md hover:shadow-lg transition-all duration-300"
+                  >
+                    <span className="text-xs">
+                      {sortOption === "lowToHigh"
+                        ? "Price (Low to High)"
+                        : sortOption === "highToLow"
+                        ? "Price (High to Low)"
+                        : sortOption === "nameAZ"
+                        ? "Name (A-Z)"
+                        : "Sort By"}
+                    </span>
+                    <FaAngleDown className="text-xs lg:text-xl" />
+                  </button>
+
+                  {isSortOpen && (
+                    <div className="absolute z-10 mt-2 w-full bg-white border border-black rounded-md shadow-lg text-sm">
+                      <div
+                        onClick={() => {
+                          setSortOption("lowToHigh");
+                          setIsSortOpen(false);
+                        }}
+                        className="px-4 py-2 text-xs hover:bg-gray-100 cursor-pointer"
+                      >
+                        Price (Low to High)
+                      </div>
+                      <div
+                        onClick={() => {
+                          setSortOption("highToLow");
+                          setIsSortOpen(false);
+                        }}
+                        className="px-4 py-2  text-xs hover:bg-gray-100 cursor-pointer"
+                      >
+                        Price (High to Low)
+                      </div>
+                      <div
+                        onClick={() => {
+                          setSortOption("nameAZ");
+                          setIsSortOpen(false);
+                        }}
+                        className="px-4 py-2  text-xs hover:bg-gray-100 cursor-pointer"
+                      >
+                        Name (A-Z)
+                      </div>
+                      <div
+                        onClick={() => {
+                          setSortOption(null);
+                          setIsSortOpen(false);
+                        }}
+                        className="px-4 py-2  text-xs text-red-500 hover:bg-gray-100 cursor-pointer"
+                      >
+                        Clear Sort
+                      </div>
+                    </div>
+                  )}
+                </div>
+
+                {/* Categories */}
+                <div>
+                  <h3 className="text-black not-italic font-semibold text-[24px] mb-8">
+                    Categories
+                  </h3>
+
+                  {categoryData.map((cat) => (
+                    <div key={cat.name} className="mb-4">
+                      <h4 className="text-black font-medium cursor-pointer text-[18px] mb-6">
+                        {cat.name}
+                      </h4>
+                      {cat.subCategories.map((sub) => (
+                        <div
+                          key={sub}
+                          className="flex items-center cursor-pointer gap-2 mb-4"
+                        >
+                          <input
+                            type="radio"
+                            name="subcategory"
+                            className="w-4 h-4 cursor-pointer accent-black"
+                            checked={selectedSubCategory === sub}
+                            onChange={() => {
+                              setSelectedCategory(cat.name);
+                              setSelectedSubCategory(sub);
+                            }}
+                          />
+                          <label className="text-black font-light text-[14px] leading-normal">
+                            {sub}
+                          </label>
+                        </div>
+                      ))}
+                    </div>
+                  ))}
+
+                  {(selectedCategory || selectedSubCategory) && (
+                    <button
+                      onClick={() => {
+                        setSelectedCategory(null);
+                        setSelectedSubCategory(null);
+                      }}
+                      className="mt-4 text-sm underline cursor-pointer text-blue-600"
+                    >
+                      Clear Category Filter
+                    </button>
+                  )}
+                </div>
+              </div>
+            </div>
+          </div>
+        )}
       </Container>
     </>
   );
