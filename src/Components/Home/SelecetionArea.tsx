84a3534f7066c03271f712b4c5ea7dc9d13d77b4
fahim-sarker <sarkerfahim599@gmail.com>
2025-05-21 11:06:40 +0600

    fix button issues

--- src/Components/Home/SelecetionArea.tsx
+++ src/Components/Home/SelecetionArea.tsx
@@ -19,7 +19,10 @@ const SelectionArea = () => {
           <CustomSelect options={options} placeholder="Decor" />
           <CustomSelect options={options} placeholder="Lighting" />
           <CustomSelect options={options} placeholder="Style" />
-          <button className="rounded-[24px] bg-[#000] text-white outline-0 text-[16px] font-inter cursor-pointer py-4 px-10 w-[300px] hover:bg-[#1a1a1a] transition-all duration-300">
+          <button
+            className="rounded-[24px] bg-[#000] text-white outline-0 text-[16px] font-inter cursor-pointer py-4 px-10 w-[300px] 
+          hover:bg-[#1a1a1a] transition-all duration-300"
+          >
             Apply
           </button>
         </div>
