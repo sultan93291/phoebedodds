1e3735764fd1740b28971ab7948281dd29d252de
fahim-sarker <sarkerfahim599@gmail.com>
2025-05-22 09:09:24 +0600

    check

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,9 +1,9 @@
-import Newslater from "@/Components/Home/Newslater";
+import Room from "../Components/Home/Room";
 import About from "../Components/Home/About";
 import Banner from "../Components/Home/Banner";
 import Feature from "../Components/Home/Feature";
+import Newslater from "@/Components/Home/Newslater";
 import OurPartners from "../Components/Home/OurPartners";
-import Room from "../Components/Home/Room";
 import SelcetionArea from "../Components/Home/SelecetionArea";
 
 const Home = () => {
