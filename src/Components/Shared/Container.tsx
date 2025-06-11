5cd6c17ff36601827e44ae79da3e3562ce466e37
Naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2025-06-11 22:19:20 +0600

    redux init

--- src/Components/Shared/Container.tsx
+++ src/Components/Shared/Container.tsx
@@ -7,7 +7,11 @@ interface Containerprops {
 
 const Container: React.FC<Containerprops> = ({ children, className }) => {
   return (
-    <div className={`max-w-[1680px] mx-auto ${className || ""}`}>
+    <div
+      className={`max-w-[1450px] mx-auto 3xl:max-w-[1680px] overflow-x-hidden ${
+        className || ""
+      }`}
+    >
       {children}
     </div>
   );
