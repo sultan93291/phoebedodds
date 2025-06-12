0b7849d7396247652800fb5322ef19e8ee2f26c3
naymurrahmanshahed <naymurrahmanshahed@gmail.com>
2025-06-12 12:37:50 +0600

    partner slice added

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import bannerReducer from "../features/cms/banner/bannerSlice";
 import aboutReducer from "../features/cms/about/aboutSlice";
+import partnerReducer from "@/features/cms/partners/partnerSlice";
 
 export const store = configureStore({
   reducer: {
     banner: bannerReducer,
     about: aboutReducer,
+    partner: partnerReducer,
   },
 });
 
